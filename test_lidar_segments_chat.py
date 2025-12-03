5535bee539b3740835e3a17fcee1bc39a330612f
raffy952 <ragnoraffaele162@gmail.com>
2025-12-03 17:57:50 +0100

    aggiunta predizione della posa

--- test_lidar_segments_chat.py
+++ test_lidar_segments_chat.py
@@ -7,6 +7,7 @@ from scansegmentapi.udp_handler import UDPHandler
 import keyboard
 import math
 import joblib
+from tensorflow.keras.models import load_model
 from feature_extractor import calculate_cluster_features
 import warnings
 warnings.filterwarnings("ignore", message="X does not have valid feature names")
@@ -21,8 +22,11 @@ transport = UDPHandler(UDP_IP, UDP_PORT, 65535)
 receiver = CompactApi.Receiver(transport)
 
 # Carica modello
-model_path = 'best_params.pkl'
+model_path = 'best_params_180.pkl'
 model = joblib.load(model_path)
+scaler = joblib.load('scaler_y.pkl')
+model_reg = load_model('model_final_20000.h5')
+labels_to_keep = [1, 2]
 
 # Ordine delle feature utilizzate dal modello
 FEATURE_ORDER = [
@@ -40,6 +44,18 @@ frame_numbers = []
 segment_counters = []
 
 
+def pad_center(arr, X):
+    n = len(arr)
+    if n >= X:
+        return arr[:X]   # opzionale: tronca se troppo lungo
+
+    total_pad = X - n
+    left = total_pad // 2
+    right = total_pad - left   # garantisce correttezza anche per X dispari
+
+    return np.pad(arr, (left, right), mode='constant', constant_values=0)
+
+
 # ===============================
 # LOOP PRINCIPALE
 # ===============================
@@ -50,9 +66,8 @@ while True:
     segments.append(segment[0])
     frame_numbers.append(frame_number[0])
     segment_counters.append(segment_counter[0])
-
     # Controlla se abbiamo 10 segmenti dello stesso frame in ordine
-    if len(frame_numbers) == 10 and len(segment_counters) == 10:
+    if len(frame_numbers) == 6 and len(segment_counters) == 6:
         if np.all(np.array(frame_numbers) == frame_numbers[0]):
             if np.all(np.array(segment_counters[:-1]) < np.array(segment_counters[1:])):
 
@@ -73,8 +88,9 @@ while True:
 
                 x = distances * np.cos(thetas + math.pi/2) / 1000.0
                 y = distances * np.sin(thetas + math.pi/2) / 1000.0
-
+                
                 pts = np.column_stack((x.flatten(), y.flatten()))
+                print(f"Numero punti scan ricostruito: {pts.shape[0]}")
 
                 # ===============================
                 # DBSCAN
@@ -105,7 +121,7 @@ while True:
                         continue
 
                     # Cluster troppo piccoli → salto PCA e ConvexHull
-                    if len(cluster_pts) < 3:
+                    if len(cluster_pts) < 3 or np.all(cluster_pts == 0.0) or np.all(cluster_pts < 0.05):
                         print(f"Cluster {lab}: troppo piccolo ({len(cluster_pts)} punti). Saltato.")
                         continue
 
@@ -125,10 +141,20 @@ while True:
 
                     #print(f"Cluster {lab} → Class: {classification} | Feature vector: {feature_vector}")
 
+                    
+
                     # ===============================
                     # DISEGNA CLUSTER
                     # ===============================
                     plt.scatter(cluster_pts[:, 0], cluster_pts[:, 1], s=8, color=col, label=f"{classification} {prob:.3f}")
+                
+                filtered_points = np.where(~np.isin(labels, labels_to_keep)[:, None], pts, 0)
+                filtered_distances = np.linalg.norm(filtered_points, axis=1)
+                filtered_distances_padded = pad_center(filtered_distances, 1105).reshape(1,-1)
+                prediction = model_reg.predict(filtered_distances_padded / 60)
+                prediction = scaler.inverse_transform(prediction)   
+                plt.scatter(prediction[0,0], prediction[0,1], color='red', alpha=1.0, marker='x')
+                plt.scatter(filtered_points[:, 1], filtered_points[:, 0], s=2, color='gray', alpha=0.3, label="Filtered points")
 
                 plt.legend(loc="upper right", fontsize=8)
                 plt.show()
@@ -137,6 +163,7 @@ while True:
                 frame_numbers = []
                 segment_counters = []
                 segments = []
+
 
         else:
             mask = np.array(frame_numbers) != frame_numbers[0]
