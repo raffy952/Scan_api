a64b9867707e901dfe326d9b605743c77359a8b7
raffy952 <ragnoraffaele162@gmail.com>
2026-01-20 18:06:01 +0100

    tolti alcuni file inutili

--- import numpy as np_1.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import pandas as pd
-
-print(np.load('filtered_distances.npy'))
-pd.DataFrame(np.load('filtered_distances.npy')).to_csv('filtered_distances.csv', index=False)
--- import pandas as pd.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-
-print(np.load('scans/scan_0000.npz', allow_pickle=True)['distances'])
-print(np.load('scans/scan_0000.npz', allow_pickle=True)['theta'])
